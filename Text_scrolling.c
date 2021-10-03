02437a4575a341184335cdc16e913511811e88eb
naveenvoleti <39459453+naveenvoleti@users.noreply.github.com>
2021-10-03 10:48:18 +0530

    Update Text_scrolling.c

--- Text_scrolling.c
+++ Text_scrolling.c
@@ -5,7 +5,9 @@
 
 void delay(int number_of_seconds)
 
-{ int milli_seconds = 1000 * number_of_seconds; clock_t start_time = clock(); while (clock() < start_time +
+{ int milli_seconds = 1000 * number_of_seconds; 
+ clock_t start_time = clock(); 
+ while (clock() < start_time +
 
 milli_seconds);
 
@@ -17,9 +19,11 @@ int main(void)
 
 char data[] = "12345678";
 
-for(uint16_t i = 0; i < strlen(data)-3; i++){ char * prr_str_start = &data[i];
+for(uint16_t i = 0; i < strlen(data)-3; i++){ 
+  char * prr_str_start = &data[i];
 
-for(uint16_t j = 0; j < 4; j++){ printf("%c",prr_str_start[j]);
+for(uint16_t j = 0; j < 4; j++){ 
+  printf("%c",prr_str_start[j]);
 
 }
 
